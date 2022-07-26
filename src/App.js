0ee92cd0499cb873afc3a6fb443072a33c74f209
elazzaouy-salaheddine <salaheddine.elazzaouy@outlook.com>
2022-07-26 22:44:00 +0100

    home page part 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/home/Home";
 import List from "./pages/list/List";
 import Registrations from "./pages/registrations/Registrations";
@@ -7,12 +9,22 @@ import Add from "./pages/add/Add";
 function App() {
   return (
     <>
-      <div>App</div>
-      <Home />
-      <List />
-      <Registrations />
-      <Single />
-      <Add />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="register" element={<Registrations />} />
+        <Route path="products">
+          <Route index element={<List />} />
+          <Route path=":productsId" element={<Single />} />
+          <Route path="add" element={<Add />} />
+        </Route>
+
+        <Route path="users">
+          <Route index element={<List />} />
+          <Route path=":usersId" element={<Single />} />
+          <Route path="add" element={<Add />} />
+        </Route>
+
+      </Routes>
     </>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
-import './home.scss'
+import React from "react";
+import Sidebar from "../../comp/sidebar/Sidebar";
+import Navbar from "../../comp/navbar/Navbar";
+import Widget from '../../comp/widget/Widget';
+import Featured from "../../comp/featured/Featured";
+import "./home.scss";
 const Home = () => {
   return (
-    <div className='home'>Home</div>
-  )
-}
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="user" />
+          <Widget type="order" />
+          <Widget type="earning" />
+          <Widget type="balance" />
+        </div>
+        <div className="charts">
+          <Featured />
+          
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
