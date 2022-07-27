926605dd5251c318435fbc9c93692b17ee8ede3b
elazzaouy-salaheddine <salaheddine.elazzaouy@outlook.com>
2022-07-27 22:32:57 +0100

    add charts in home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -3,6 +3,8 @@ import Sidebar from "../../comp/sidebar/Sidebar";
 import Navbar from "../../comp/navbar/Navbar";
 import Widget from '../../comp/widget/Widget';
 import Featured from "../../comp/featured/Featured";
+import Chart from "../../comp/chart/Chart";
+import Table from "../../comp/table/Table";
 import "./home.scss";
 const Home = () => {
   return (
@@ -18,8 +20,16 @@ const Home = () => {
         </div>
         <div className="charts">
           <Featured />
-          
+          <Chart title="Last 6 Months (Revenue)" aspect={2 / 1} />
         </div>
+
+
+        <div className="listContainer">
+          <div className="listTitle">Latest Transactions</div>
+          <Table />
+        </div>
+
+
       </div>
     </div>
   );
